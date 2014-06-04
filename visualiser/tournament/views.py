daf83d8306a94f3c74699834c35d1d9e8cb34e2a
Chris Brand <chris_brand@shaw.ca>
2014-06-03 22:33:04 -0700

    Add some initial tournament views
    Still lots of stubs.

--- visualiser/tournament/views.py
+++ visualiser/tournament/views.py
@@ -14,11 +14,50 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("This is the index")
+from tournament.models import Tournament, Round, Game
 
-def detail(request, tournament_id):
-    return HttpResponse("This is the tournament %s" % tournament_id)
+def index(request):
+    the_list = Tournament.objects.order_by('-start_date')
+    context = {'tournament_list': the_list}
+    return render(request, 'tournaments/index.html', context)
+
+def tournament_detail(request, tournament_id):
+    t = get_object_or_404(Tournament, pk=tournament_id)
+    return render(request, 'tournaments/detail.html', {'tournament': t})
+
+def tournament_scores(request, tournament_id):
+    t = get_object_or_404(Tournament, pk=tournament_id)
+    return render(request, 'tournaments/scores.html', {'tournament': t})
+
+def tournament_round(request, tournament_id):
+    return HttpResponse("This is the tournament %s current round" % tournament_id)
+
+def round_index(request, tournament_id):
+    the_list = Tournament.objects.order_by('-start_date')
+    context = {'round_list': the_list}
+    return render(request, 'rounds/index.html', context)
+
+def round_detail(request, tournament_id, round_num):
+    return HttpResponse("This is the tournament %s round %s detail" % (tournament_id, round_num))
+
+def round_scores(request, tournament_id, round_num):
+    return HttpResponse("This is the tournament %s round %s scores" % (tournament_id, round_num))
+
+def game_index(request, tournament_id, round_num):
+    return HttpResponse("This is the tournament %s round %s game index" % (tournament_id, round_num))
+
+def game_detail(request, tournament_id, game_name):
+    return HttpResponse("This is the tournament %s game %s detail" % (tournament_id, game_name))
+
+def game_sc_chart(request, tournament_id, game_name):
+    return HttpResponse("This is the tournament %s game %s sc_chart" % (tournament_id, game_name))
+
+def game_news(request, tournament_id, game_name):
+    return HttpResponse("This is the tournament %s game %s news" % (tournament_id, game_name))
+
+def game_background(request, tournament_id, game_name):
+    return HttpResponse("This is the tournament %s game %s background" % (tournament_id, game_name))
+
