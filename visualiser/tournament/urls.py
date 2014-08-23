3d2f09419ca0a9f79f64b11e45c4f9eca2465a4c
Chris Brand <chris_brand@shaw.ca>
2014-08-23 00:34:48 -0700

    Restructure using include to avoid repetition

--- visualiser/tournament/urls.py
+++ visualiser/tournament/urls.py
@@ -14,24 +14,36 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 
 from tournament import views
 
 urlpatterns = patterns('',
     url(r'^$', views.TourneyIndexView.as_view(), name='index'),
     url(r'^(?P<pk>\d+)/$', views.TourneyDetailView.as_view(), name='tournament_detail'),
-    url(r'^(?P<tournament_id>\d+)/scores/$', views.tournament_scores, name='tournament_scores'),
-    url(r'^(?P<tournament_id>\d+)/current_round/$', views.tournament_round, name='tournament_round'),
-    url(r'^(?P<tournament_id>\d+)/news/$', views.tournament_news, name='tournament_news'),
-    url(r'^(?P<tournament_id>\d+)/background/$', views.tournament_background, name='tournament_background'),
-    url(r'^(?P<tournament_id>\d+)/rounds/$', views.round_index, name='round_index'),
-    url(r'^(?P<tournament_id>\d+)/rounds/(?P<round_num>\d+)/$', views.round_detail, name='round_detail'),
-    url(r'^(?P<tournament_id>\d+)/rounds/(?P<round_num>\d+)/scores/$', views.round_scores, name='round_scores'),
-    url(r'^(?P<tournament_id>\d+)/rounds/(?P<round_num>\d+)/roll_call/$', views.roll_call, name='roll_call'),
-    url(r'^(?P<tournament_id>\d+)/rounds/(?P<round_num>\d+)/games/$', views.game_index, name='game_index'),
-    url(r'^(?P<tournament_id>\d+)/games/(?P<game_name>\w+)/$', views.game_detail, name='game_detail'),
-    url(r'^(?P<tournament_id>\d+)/games/(?P<game_name>\w+)/sc_chart/$', views.game_sc_chart, name='game_sc_chart'),
-    url(r'^(?P<tournament_id>\d+)/games/(?P<game_name>\w+)/news/$', views.game_news, name='game_news'),
-    url(r'^(?P<tournament_id>\d+)/games/(?P<game_name>\w+)/background/$', views.game_background, name='game_background'),
+    url(r'^(?P<tournament_id>\d+)/', include([
+        url(r'^scores/$', views.tournament_scores, name='tournament_scores'),
+        url(r'^enter_scores/$', views.round_scores, name='enter_scores'),
+        url(r'^roll_call/$', views.roll_call, name='roll_call'),
+        url(r'^current_round/$', views.tournament_round, name='tournament_round'),
+        url(r'^news/$', views.tournament_news, name='tournament_news'),
+        url(r'^background/$', views.tournament_background, name='tournament_background'),
+        url(r'^rounds/$', views.round_index, name='round_index'),
+        # TODO can probably move this one into the block below, too
+        url(r'^rounds/(?P<round_num>\d+)/$', views.round_detail, name='round_detail'),
+        url(r'^rounds/(?P<round_num>\d+)/', include([
+            url(r'^create_games/$', views.create_games, name='create_games'),
+            url(r'^game_scores/$', views.game_scores, name='game_scores'),
+            url(r'^games/$', views.game_index, name='game_index'),
+        ])),
+        # TODO can probably move this one into the block below, too
+        url(r'^games/(?P<game_name>\w+)/$', views.game_detail, name='game_detail'),
+        url(r'^games/(?P<game_name>\w+)/', include([
+            url(r'^sc_chart/$', views.game_sc_chart, name='game_sc_chart'),
+            url(r'^enter_scs/$', views.sc_counts, name='enter_scs'),
+            url(r'^news/$', views.game_news, name='game_news'),
+            url(r'^background/$', views.game_background, name='game_background'),
+            url(r'^draw_vote/$', views.draw_vote, name='draw_vote'),
+        ])),
+    ])),
 )
