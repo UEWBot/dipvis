8478b175ae0c869c82e6b29f713e8526b8b5d437
Chris Brand <chris.brand@broadcom.com>
2016-08-11 17:06:08 -0700

    Views: Convert TODOs to explanations
    There's actually no work needed in these two places.

--- visualiser/tournament/views.py
+++ visualiser/tournament/views.py
@@ -237,7 +237,8 @@ class BasePlayerRoundFormset(BaseFormSet):
         # Pass the three special args down to the form itself
         kwargs['tournament'] = self.tournament
         kwargs['rounds'] = self.tournament.round_set.count()
-        # TODO current_round() could return None, if all rounds are over
+        # current_round() could return None, if all rounds are over,
+        # but that just gives us a blank form, which is fine
         kwargs['this_round'] = self.tournament.current_round().number
         return super(BasePlayerRoundFormset, self)._construct_form(index, **kwargs)
 
@@ -296,7 +297,8 @@ class BasePlayerRoundScoreFormset(BaseFormSet):
         # Pass the three special args down to the form itself
         kwargs['tournament'] = self.tournament
         kwargs['rounds'] = self.tournament.round_set.count()
-        # TODO current_round() could return None, if all rounds are over
+        # current_round() could return None, if all rounds are over,
+        # but that just gives us a blank form, which is fine
         kwargs['this_round'] = self.tournament.current_round().number
         return super(BasePlayerRoundScoreFormset, self)._construct_form(index, **kwargs)
 
