b6a20b7e4cf0a3c632b757ad9268005ec5194817
Chris Brand <chris_brand@shaw.ca>
2018-10-15 03:50:26 +0000

    Settings: Middleware_Classes -> Middleware
    Django 1.10 introduced MIDDLEWARE.

--- visualiser/visualiser/settings.py
+++ visualiser/visualiser/settings.py
@@ -61,7 +61,7 @@ INSTALLED_APPS = (
     'django_extensions',
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
