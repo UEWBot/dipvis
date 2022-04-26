32fd42fdd82bdb0e9fa0989ffdf43d1ec05a4024
Jason <jason.mastbaum@gmail.com>
2022-04-25 22:35:13 -0700

    put the license info back at the top of all the refactored files

--- visualiser/tournament/diplomacy/values/diplomacy_values.py
+++ visualiser/tournament/diplomacy/values/diplomacy_values.py
@@ -1,3 +1,19 @@
+# Diplomacy Tournament Visualiser
+# Copyright (C) 2020 Chris Brand
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 TOTAL_SCS = 34
 WINNING_SCS = ((TOTAL_SCS // 2) + 1)
 FIRST_YEAR = 1901
