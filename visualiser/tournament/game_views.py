UEWBot/dipvis

commit fde716880779c593a857fa15e04b9428a678ac7c

Game views: Convert year to int earlier

visualiser/tournament/game_views.py
@@ -482,7 +482,7 @@ def game_news(request, tournament_id, game_name, for_year=None, as_ticker=False)
     context = {'tournament': t,
                'game': g,
                'subject': 'News',
-               'content': news(g, for_year)}
+               'content': news(g, int(for_year))}
     if as_ticker:
         context['redirect_time'] = REFRESH_TIME
         context['redirect_url'] = reverse('game_ticker',

visualiser/tournament/news.py
@@ -264,7 +264,7 @@ def _game_news(g, include_game_name=False, mask=MASK_ALL_NEWS, for_year=None):
         return [g.result_str(include_game_name) + '.']
     centres_set = g.centrecount_set.order_by('-year')
     if for_year:
-        last_year = int(for_year)
+        last_year = for_year
         centres_set = centres_set.filter(year__lte=last_year)
     else:
         # Which is the most recent year we have info for ?

commit f88eaee954ed0c45b470f7321ad9e4967cd2930b

Views: Support game_news with no year
Commit fde716880779c593a857fa15e04b9428a678ac7c
("Game views: Convert year to int earlier") broke support for passing
None as the year. This patch restores that functionality.

visualiser/tournament/game_views.py
@@ -482,7 +482,7 @@ def game_news(request, tournament_id, game_name, for_year=None, as_ticker=False)
     context = {'tournament': t,
                'game': g,
                'subject': 'News',
-               'content': news(g, int(for_year))}
+               'content': news(g, int(for_year) if for_year else None)}
     if as_ticker:
         context['redirect_time'] = REFRESH_TIME
         context['redirect_url'] = reverse('game_ticker',