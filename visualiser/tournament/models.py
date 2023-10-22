UEWBot/dipvis

commit 241ece25997a1c4a3d3933989ea217b8dc02a764

Use pathlib.Path where appropriate
Issue #261

visualiser/tournament/models.py
@@ -21,7 +21,7 @@ from abc import ABC, abstractmethod
 from datetime import datetime, timedelta
 import inspect
 from operator import itemgetter
-import os
+from pathlib import Path
 import random
 import string
 import uuid
@@ -530,8 +530,7 @@ def game_image_location(instance, filename):
     # We expect instance to be a GameImage
     game = instance.game
     tournament = game.the_round.tournament
-    directory = os.path.join(tournament.name, str(tournament.start_date), game.name)
-    return os.path.join('games', directory, filename)
+    directory = Path('games', tournament.name, str(tournament.start_date), game.name, filename)
 
 
 class Award(models.Model):

visualiser/tournament/diplomacy/utils/game_image_location.py
@@ -1,7 +1,7 @@
-import os
+from pathlib import Path
 
 def game_image_location(instance, filename):
     """
     Function that determines where to store the file.
     """
-    return os.path.join('games', 'starting_positions', filename)
+    return Path('games/starting_positions', filename)

visualiser/visualiser/settings.py
@@ -8,9 +8,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve(strict=True).parents[1]
 
 
 # Quick-start development settings - unsuitable for production
@@ -28,7 +29,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates')
+            BASE_DIR / 'templates',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -102,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -140,7 +141,7 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 LOCALE_PATHS = [
-    os.path.join(BASE_DIR, 'locale'),
+    BASE_DIR / 'locale',
 ]
 
 STATIC_URL = '/static/'

commit 0891e65800b6f977ec55e6588fb4b6b7dfc432e8

Fix game_image_location()
Fixes a bug introduced in commit 241ece25997a1c4a3d3933989ea217b8dc02a764

visualiser/tournament/models.py
@@ -530,7 +530,7 @@ def game_image_location(instance, filename):
     # We expect instance to be a GameImage
     game = instance.game
     tournament = game.the_round.tournament
-    directory = Path('games', tournament.name, str(tournament.start_date), game.name, filename)
+    return Path('games', tournament.name, str(tournament.start_date), game.name, filename)
 
 
 class Award(models.Model):