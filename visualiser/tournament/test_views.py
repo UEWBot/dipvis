face844d6ab80bb757608e491b4b020d1f9b3013
Chris Brand <chris.carter.brand@gmail.com>
2023-11-28 20:18:41 -0800

    Test: Add tests of empty index pages

---
+++ visualiser/tournament/test_views.py
@@ -0,0 +1,88 @@
+# Diplomacy Tournament Visualiser
+# Copyright (C) 2023 Chris Brand
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from django.test import TestCase
+from django.urls import reverse
+from django.utils import timezone
+
+import tournament
+
+from tournament.models import G_SCORING_SYSTEMS, R_SCORING_SYSTEMS, T_SCORING_SYSTEMS
+from tournament.models import DrawSecrecy, Round, Tournament
+
+class ViewIndexTests(TestCase):
+    """Test the various index views with no objects of that type"""
+
+    def test_series_index(self):
+        response = self.client.get(reverse('series_index'),
+                                   secure=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_index(self):
+        response = self.client.get(reverse('index'),
+                                   secure=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_tournamentplayer_index(self):
+        now = timezone.now()
+        t = Tournament.objects.create(name='A Tournament',
+                                      start_date=now,
+                                      end_date=now,
+                                      round_scoring_system=R_SCORING_SYSTEMS[0].name,
+                                      tournament_scoring_system=T_SCORING_SYSTEMS[0].name,
+                                      draw_secrecy=DrawSecrecy.SECRET,
+                                      is_published=True)
+        response = self.client.get(reverse('tournament_players',
+                                           args=(t.id,)),
+                                   secure=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_round_index(self):
+        now = timezone.now()
+        t = Tournament.objects.create(name='A Tournament',
+                                      start_date=now,
+                                      end_date=now,
+                                      round_scoring_system=R_SCORING_SYSTEMS[0].name,
+                                      tournament_scoring_system=T_SCORING_SYSTEMS[0].name,
+                                      draw_secrecy=DrawSecrecy.SECRET,
+                                      is_published=True)
+        response = self.client.get(reverse('round_index',
+                                           args=(t.id,)),
+                                   secure=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_game_index(self):
+        now = timezone.now()
+        t = Tournament.objects.create(name='A Tournament',
+                                      start_date=now,
+                                      end_date=now,
+                                      round_scoring_system=R_SCORING_SYSTEMS[0].name,
+                                      tournament_scoring_system=T_SCORING_SYSTEMS[0].name,
+                                      draw_secrecy=DrawSecrecy.SECRET,
+                                      is_published=True)
+        Round.objects.create(tournament=t,
+                             scoring_system=G_SCORING_SYSTEMS[0].name,
+                             dias=False,
+                             start=t.start_date)
+        response = self.client.get(reverse('game_index',
+                                           args=(t.id, 1)),
+                                   secure=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_player_index(self):
+        response = self.client.get(reverse('player_index'),
+                                   secure=True)
+        self.assertEqual(response.status_code, 200)
