a8d3bc2892a87fe33f527f632e17f68bbabf6820
Chris Brand <chris.carter.brand@gmail.com>
2025-10-13 14:55:00 -0700

    Tests: Move tests of TournamentGameState to separate file

--- visualiser/tournament/test_tournament_game_state.py
+++ visualiser/tournament/test_tournament_game_state.py
@@ -1,16 +1,16 @@
 # Diplomacy Tournament Visualiser
 # Copyright (C) 2014, 2016-2020 Chris Brand
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -22,7 +22,6 @@ from tournament.diplomacy.models.game_set import GameSet
 from tournament.diplomacy.models.great_power import GreatPower
 from tournament.game_scoring.g_scoring_systems import G_SCORING_SYSTEMS
 from tournament.game_scoring.base import InvalidYear
-from tournament.game_scoring.simple_game_state import SimpleGameState
 from tournament.models import Tournament, Round, Game, DrawProposal, CentreCount
 from tournament.models import R_SCORING_SYSTEMS, T_SCORING_SYSTEMS
 from tournament.models import DrawSecrecy, Seasons
@@ -32,32 +31,9 @@ from tournament.tournament_game_state import TournamentGameState
 HOURS_24 = timedelta(hours=24)
 
 
-# Function needed by most classes
-def check_score_order(self, scores):
-    """Check that the scores appear in GreatPower order when iterated through"""
-    EXPECT = [p for p in GreatPower.objects.all()]
-    order = [k for k in scores.keys()]
-    self.assertEqual(EXPECT, order)
-
-
-def check_score_for_state(self, state, system_name, expected_scores):
+class TournamentGameStateTests(TestCase):
     """
-    Generic test for a scoring system
-
-    Checks the scores produced by the specified scoring system for the specifed game state.
-    """
-    system = find_game_scoring_system(system_name)
-    scores = system.scores(state)
-    self.assertEqual(7, len(scores))
-    for p,s in scores.items():
-        with self.subTest(power=p):
-            self.assertAlmostEqual(s, expected_scores[p])
-    check_score_order(self, scores)
-
-
-class GameScoringTests(TestCase):
-    """
-    Tests that aren't specific to one scoring system
+    Test TournamentGameState
     """
     fixtures = ['game_sets.json', 'players.json']
 
@@ -161,78 +137,96 @@ class GameScoringTests(TestCase):
         CentreCount.objects.create(power=cls.italy, game=g11, year=1907, count=0)
         CentreCount.objects.create(power=cls.russia, game=g11, year=1907, count=5)
         CentreCount.objects.create(power=cls.turkey, game=g11, year=1907, count=7)
-        # SimpleGameStates for two Games
-        cls.three_way_tie = SimpleGameState(sc_counts={cls.austria: 1,
-                                                       cls.england: 10,
-                                                       cls.france: 1,
-                                                       cls.germany: 1,
-                                                       cls.italy: 10,
-                                                       cls.russia: 10,
-                                                       cls.turkey: 1},
-                                            final_year=1907,
-                                            elimination_years={},
-                                            draw=None)
-        cls.three_survivors = SimpleGameState(sc_counts={cls.austria: 0,
-                                                         cls.england: 17,
-                                                         cls.france: 0,
-                                                         cls.germany: 0,
-                                                         cls.italy: 16,
-                                                         cls.russia: 1,
-                                                         cls.turkey: 0},
-                                              final_year=1907,
-                                              elimination_years={cls.austria: 1903,
-                                                                 cls.france: 1907,
-                                                                 cls.germany: 1905,
-                                                                 cls.turkey: 1905},
-                                              draw=None)
 
-    def test_no_corruption(self):
-        """Ensure that calls to calculate scores are independent"""
-        # Essentially a test for issue #188
-        system = find_game_scoring_system('CDiplo 80')
-        scores = system.scores(self.three_way_tie)
-        self.assertEqual(7, len(scores))
-        for p,s in scores.items():
-            sc = self.three_way_tie.dot_count(p)
-            # 3 powers equal on 10 SCs, and 4 equal on 1 SC
-            if sc == 1:
-                self.assertEqual(s, 1)
-            else:
-                self.assertEqual(s, (25 + 14 + 7) / 3 + 10)
-        self.assertEqual(sum(scores.values()), 80)
-        scores = system.scores(self.three_survivors)
-        self.assertEqual(7, len(scores))
-        for p,s in scores.items():
-            sc = self.three_survivors.dot_count(p)
-            if sc == 17:
-                self.assertEqual(s, 17 + 25)
-            elif sc == 16:
-                self.assertEqual(s, 16 + 14)
-            elif sc == 1:
-                self.assertEqual(s, 1 + 7)
-            else:
-                self.assertEqual(s, 0)
-        self.assertEqual(sum(scores.values()), 80)
-
-    # description for all G_SCORING_SYSTEMS
-    def test_description(self):
-        for system in G_SCORING_SYSTEMS:
-            with self.subTest(system=system.name):
-                desc = system.description
-                # TODO verify desc
-
-    # dead_score_can_change() for all G_SCORING_SYSTEMS
-    def test_score_changes(self):
-        """Compare score for eliminated power before and after a solo"""
+    def test_tgs_concession_is_solo(self):
         t = Tournament.objects.get(name='t1')
         g = t.round_numbered(1).game_set.get(name='g11')
-        scs1 = g.centrecount_set.filter(year__lte=1906)
-        tgs1 = TournamentGameState(scs1)
-        scs2 = g.centrecount_set.filter(year__lte=1907)
-        tgs2 = TournamentGameState(scs2)
-        for system in G_SCORING_SYSTEMS:
-            with self.subTest(system=system.name):
-                scores1 = system.scores(tgs1)
-                scores2 = system.scores(tgs2)
-                changes = scores1[self.france] != scores2[self.france]
-                self.assertEqual(changes, system.dead_score_can_change)
+        self.assertIsNone(g.passed_draw())
+        scs = g.centrecount_set.filter(year__lte=1901)
+        # Add a passed draw
+        dp = DrawProposal.objects.create(game=g,
+                                         year=1901,
+                                         season=Seasons.SPRING,
+                                         passed=True,
+                                         proposer=self.austria)
+        dp.drawing_powers.add(self.france)
+        tgs = TournamentGameState(scs)
+        system = find_game_scoring_system('Solo or bust')
+        scores = system.scores(tgs)
+        self.assertEqual(7, len(scores))
+        for p,s in scores.items():
+            if p == self.france:
+                self.assertEqual(s, 100)
+            else:
+                self.assertEqual(s, 0)
+        self.assertEqual(tgs.solo_year(), dp.year)
+        # Clean up
+        dp.delete()
+
+    def test_tgs_draw_is_not_solo(self):
+        t = Tournament.objects.get(name='t1')
+        g = t.round_numbered(1).game_set.get(name='g11')
+        self.assertIsNone(g.passed_draw())
+        scs = g.centrecount_set.filter(year__lte=1901)
+        # Add a passed draw
+        dp = DrawProposal.objects.create(game=g,
+                                         year=1901,
+                                         season=Seasons.SPRING,
+                                         passed=True,
+                                         proposer=self.austria)
+        dp.drawing_powers.add(self.france)
+        dp.drawing_powers.add(self.turkey)
+        tgs = TournamentGameState(scs)
+        system = find_game_scoring_system('Solo or bust')
+        scores = system.scores(tgs)
+        self.assertEqual(7, len(scores))
+        for s in scores.values():
+            self.assertEqual(s, 0)
+        self.assertIsNone(tgs.solo_year())
+        # Clean up
+        dp.delete()
+
+    def test_tgs_solo_year_none(self):
+        t = Tournament.objects.get(name='t1')
+        g = t.round_numbered(1).game_set.get(name='g11')
+        self.assertIsNone(g.passed_draw())
+        scs = g.centrecount_set.filter(year__lte=1901)
+        tgs = TournamentGameState(scs)
+        self.assertIsNone(tgs.solo_year())
+
+    def test_tgs_dot_count_invalid_year(self):
+        t = Tournament.objects.get(name='t1')
+        g = t.round_numbered(1).game_set.get(name='g11')
+        self.assertIsNone(g.passed_draw())
+        scs = g.centrecount_set.filter(year__lte=1901)
+        tgs = TournamentGameState(scs)
+        self.assertRaises(InvalidYear, tgs.dot_count, self.france, year=1899)
+
+    def test_tgs_year_eliminated_none(self):
+        t = Tournament.objects.get(name='t1')
+        g = t.round_numbered(1).game_set.get(name='g11')
+        self.assertIsNone(g.passed_draw())
+        scs = g.centrecount_set.filter(year__lte=1901)
+        tgs = TournamentGameState(scs)
+        self.assertIsNone(tgs.year_eliminated(self.france))
+
+    def test_tgs_powers_in_draw(self):
+        t = Tournament.objects.get(name='t1')
+        g = t.round_numbered(1).game_set.get(name='g11')
+        self.assertIsNone(g.passed_draw())
+        scs = g.centrecount_set.filter(year__lte=1901)
+        # Add a passed draw
+        dp = DrawProposal.objects.create(game=g,
+                                         year=1901,
+                                         season=Seasons.SPRING,
+                                         passed=True,
+                                         proposer=self.austria)
+        dp.drawing_powers.add(self.france)
+        dp.drawing_powers.add(self.turkey)
+        tgs = TournamentGameState(scs)
+        powers = tgs.powers_in_draw()
+        self.assertEqual(len(powers), 2)
+        self.assertIn(self.france, powers)
+        self.assertIn(self.turkey, powers)
+        # Clean up
+        dp.delete()
