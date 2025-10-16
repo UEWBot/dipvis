9fc6ef4a966ece625446b4accad5d4fdafa9826b
Chris Brand <chris.carter.brand@gmail.com>
2025-10-15 17:46:49 -0700

    Game scoring: Use SCCountGameState in test_score_changes()
    This removes the need for a Game in the database.

--- visualiser/tournament/game_scoring/test_general.py
+++ visualiser/tournament/game_scoring/test_general.py
@@ -14,22 +14,16 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from datetime import date, datetime, time, timedelta, timezone
+import copy
 
 from django.test import TestCase
 
-from tournament.diplomacy.models.game_set import GameSet
 from tournament.diplomacy.models.great_power import GreatPower
 from tournament.game_scoring.g_scoring_systems import G_SCORING_SYSTEMS
 from tournament.game_scoring.game_state import InvalidYear
+from tournament.game_scoring.sc_chart_game_state import SCChartGameState
 from tournament.game_scoring.simple_game_state import SimpleGameState
-from tournament.models import Tournament, Round, Game, DrawProposal, CentreCount
-from tournament.models import R_SCORING_SYSTEMS, T_SCORING_SYSTEMS
-from tournament.models import DrawSecrecy, Seasons
 from tournament.models import find_game_scoring_system
-from tournament.tournament_game_state import TournamentGameState
-
-HOURS_24 = timedelta(hours=24)
 
 
 # Function needed by most classes
@@ -59,51 +53,10 @@ class GameScoringTests(TestCase):
     """
     Tests that aren't specific to one scoring system
     """
-    fixtures = ['game_sets.json', 'players.json']
+    fixtures = ['game_sets.json']
 
     @classmethod
     def setUpTestData(cls):
-        set1 = GameSet.objects.get(name='Avalon Hill')
-
-        s1 = G_SCORING_SYSTEMS[0].name
-
-        today = date.today()
-
-        t1 = Tournament.objects.create(name='t1',
-                                       start_date=today,
-                                       end_date=today + HOURS_24,
-                                       round_scoring_system=R_SCORING_SYSTEMS[0].name,
-                                       tournament_scoring_system=T_SCORING_SYSTEMS[0].name,
-                                       draw_secrecy=DrawSecrecy.SECRET)
-
-        # Add Rounds to t1
-        r11 = Round.objects.create(tournament=t1,
-                                   scoring_system=s1,
-                                   dias=True,
-                                   start=datetime.combine(t1.start_date, time(hour=8, tzinfo=timezone.utc)))
-        r12 = Round.objects.create(tournament=t1,
-                                   scoring_system=s1,
-                                   dias=True,
-                                   start=datetime.combine(t1.start_date, time(hour=12, tzinfo=timezone.utc)))
-        r13 = Round.objects.create(tournament=t1,
-                                   scoring_system=s1,
-                                   dias=True,
-                                   start=datetime.combine(t1.start_date, time(hour=16, tzinfo=timezone.utc)))
-        Round.objects.create(tournament=t1,
-                             scoring_system=s1,
-                             dias=True,
-                             start=datetime.combine(t1.start_date, time(hour=20, tzinfo=timezone.utc)))
-
-        # Add Games to r11
-        g11 = Game.objects.create(name='g11', started_at=r11.start, the_round=r11, the_set=set1)
-        Game.objects.create(name='g12', started_at=r11.start, the_round=r11, the_set=set1)
-        # Add Games to r12
-        Game.objects.create(name='g13', started_at=r12.start, the_round=r12, is_finished=True, the_set=set1)
-        Game.objects.create(name='g14', started_at=r12.start, the_round=r12, the_set=set1)
-        # Add Games to r13
-        Game.objects.create(name='g15', started_at=r13.start, the_round=r13, is_finished=True, the_set=set1)
-        Game.objects.create(name='g16', started_at=r13.start, the_round=r13, is_finished=True, the_set=set1)
-
         # Easy access to all the GreatPowers
         cls.austria = GreatPower.objects.get(abbreviation='A')
         cls.england = GreatPower.objects.get(abbreviation='E')
@@ -112,55 +65,59 @@ class GameScoringTests(TestCase):
         cls.italy = GreatPower.objects.get(abbreviation='I')
         cls.russia = GreatPower.objects.get(abbreviation='R')
         cls.turkey = GreatPower.objects.get(abbreviation='T')
+        powers = [cls.austria, cls.england, cls.france, cls.germany, cls.italy, cls.russia, cls.turkey]
 
-        # Add CentreCounts to g11
-        CentreCount.objects.create(power=cls.austria, game=g11, year=1901, count=5)
-        CentreCount.objects.create(power=cls.england, game=g11, year=1901, count=4)
-        CentreCount.objects.create(power=cls.france, game=g11, year=1901, count=5)
-        CentreCount.objects.create(power=cls.germany, game=g11, year=1901, count=5)
-        CentreCount.objects.create(power=cls.italy, game=g11, year=1901, count=4)
-        CentreCount.objects.create(power=cls.russia, game=g11, year=1901, count=5)
-        CentreCount.objects.create(power=cls.turkey, game=g11, year=1901, count=4)
+        # SC chart for a game that has made it to 1906 without a solo
+        pre_solo_sc_chart = {1901: {cls.austria: 5,
+                                    cls.england: 4,
+                                    cls.france: 5,
+                                    cls.germany: 5,
+                                    cls.italy: 4,
+                                    cls.russia: 5,
+                                    cls.turkey: 4},
+                             1902: {cls.austria: 4,
+                                    cls.england: 4,
+                                    cls.france: 4,
+                                    cls.germany: 6,
+                                    cls.italy: 4,
+                                    cls.russia: 6,
+                                    cls.turkey: 6},
+                             1904: {cls.austria: 0,
+                                    cls.england: 5,
+                                    cls.france: 4,
+                                    cls.germany: 8,
+                                    cls.italy: 4,
+                                    cls.russia: 5,
+                                    cls.turkey: 8},
+                             1905: {cls.austria: 0,
+                                    cls.england: 5,
+                                    cls.france: 3,
+                                    cls.germany: 13,
+                                    cls.italy: 3,
+                                    cls.russia: 4,
+                                    cls.turkey: 6},
+                             1906: {cls.austria: 0,
+                                    cls.england: 5,
+                                    cls.france: 0,
+                                    cls.germany: 17,
+                                    cls.italy: 0,
+                                    cls.russia: 5,
+                                    cls.turkey: 7}}
+        # Second SC chart that is identical but with a solo in 1907
+        post_solo_sc_chart = copy.deepcopy(pre_solo_sc_chart)
+        post_solo_sc_chart[1907] = {cls.austria: 0,
+                                    cls.england: 4,
+                                    cls.france: 0,
+                                    cls.germany: 18,
+                                    cls.italy: 0,
+                                    cls.russia: 5,
+                                    cls.turkey: 7}
 
-        CentreCount.objects.create(power=cls.austria, game=g11, year=1902, count=4)
-        CentreCount.objects.create(power=cls.england, game=g11, year=1902, count=4)
-        CentreCount.objects.create(power=cls.france, game=g11, year=1902, count=4)
-        CentreCount.objects.create(power=cls.germany, game=g11, year=1902, count=6)
-        CentreCount.objects.create(power=cls.italy, game=g11, year=1902, count=4)
-        CentreCount.objects.create(power=cls.russia, game=g11, year=1902, count=6)
-        CentreCount.objects.create(power=cls.turkey, game=g11, year=1902, count=6)
+        # One game as an SCChartGameState
+        # To cover Maxonian and 7Eleven, we need more history of the game
+        cls.pre_solo_game = SCChartGameState(powers=powers, sc_counts=pre_solo_sc_chart)
+        cls.post_solo_game = SCChartGameState(powers=powers, sc_counts=post_solo_sc_chart)
 
-        CentreCount.objects.create(power=cls.austria, game=g11, year=1904, count=0)
-        CentreCount.objects.create(power=cls.england, game=g11, year=1904, count=5)
-        CentreCount.objects.create(power=cls.france, game=g11, year=1904, count=4)
-        CentreCount.objects.create(power=cls.germany, game=g11, year=1904, count=8)
-        CentreCount.objects.create(power=cls.italy, game=g11, year=1904, count=4)
-        CentreCount.objects.create(power=cls.russia, game=g11, year=1904, count=5)
-        CentreCount.objects.create(power=cls.turkey, game=g11, year=1904, count=8)
-
-        CentreCount.objects.create(power=cls.austria, game=g11, year=1905, count=0)
-        CentreCount.objects.create(power=cls.england, game=g11, year=1905, count=5)
-        CentreCount.objects.create(power=cls.france, game=g11, year=1905, count=3)
-        CentreCount.objects.create(power=cls.germany, game=g11, year=1905, count=13)
-        CentreCount.objects.create(power=cls.italy, game=g11, year=1905, count=3)
-        CentreCount.objects.create(power=cls.russia, game=g11, year=1905, count=4)
-        CentreCount.objects.create(power=cls.turkey, game=g11, year=1905, count=6)
-
-        CentreCount.objects.create(power=cls.austria, game=g11, year=1906, count=0)
-        CentreCount.objects.create(power=cls.england, game=g11, year=1906, count=5)
-        CentreCount.objects.create(power=cls.france, game=g11, year=1906, count=0)
-        CentreCount.objects.create(power=cls.germany, game=g11, year=1906, count=17)
-        CentreCount.objects.create(power=cls.italy, game=g11, year=1906, count=0)
-        CentreCount.objects.create(power=cls.russia, game=g11, year=1906, count=5)
-        CentreCount.objects.create(power=cls.turkey, game=g11, year=1906, count=7)
-
-        CentreCount.objects.create(power=cls.austria, game=g11, year=1907, count=0)
-        CentreCount.objects.create(power=cls.england, game=g11, year=1907, count=4)
-        CentreCount.objects.create(power=cls.france, game=g11, year=1907, count=0)
-        CentreCount.objects.create(power=cls.germany, game=g11, year=1907, count=18)
-        CentreCount.objects.create(power=cls.italy, game=g11, year=1907, count=0)
-        CentreCount.objects.create(power=cls.russia, game=g11, year=1907, count=5)
-        CentreCount.objects.create(power=cls.turkey, game=g11, year=1907, count=7)
         # SimpleGameStates for two Games
         cls.three_way_tie = SimpleGameState(sc_counts={cls.austria: 1,
                                                        cls.england: 10,
@@ -224,15 +181,11 @@ class GameScoringTests(TestCase):
     # dead_score_can_change() for all G_SCORING_SYSTEMS
     def test_score_changes(self):
         """Compare score for eliminated power before and after a solo"""
-        t = Tournament.objects.get(name='t1')
-        g = t.round_numbered(1).game_set.get(name='g11')
-        scs1 = g.centrecount_set.filter(year__lte=1906)
-        tgs1 = TournamentGameState(scs1)
-        scs2 = g.centrecount_set.filter(year__lte=1907)
-        tgs2 = TournamentGameState(scs2)
         for system in G_SCORING_SYSTEMS:
             with self.subTest(system=system.name):
-                scores1 = system.scores(tgs1)
-                scores2 = system.scores(tgs2)
+                scores1 = system.scores(self.pre_solo_game)
+                scores2 = system.scores(self.post_solo_game)
+                # Pick an eliminated power, but not the first eliminated
+                # Under Haight, the first eliminated power's score can't change, but other's can
                 changes = scores1[self.france] != scores2[self.france]
                 self.assertEqual(changes, system.dead_score_can_change)
